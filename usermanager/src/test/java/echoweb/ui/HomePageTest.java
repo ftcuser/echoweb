3df4581a76090abbdac04ee957ad7d2f02265e2b
yuwang2017 <wangyu@vt.edu>
2018-04-20 11:57:03 -0400

    Add comments

--- usermanager/src/test/java/echoweb/ui/HomePageTest.java
+++ usermanager/src/test/java/echoweb/ui/HomePageTest.java
@@ -10,144 +10,149 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /*
- * Selenium Test Case
+ * A Selenium Test Case for User Manager Application
  */
 public class HomePageTest {
-	
-	
-	  private WebDriver driver;
-	  private ScreenshotHelper screenshotHelper;
-	  private WebDriverWait driverWait;
-	  
-	  private WebElement addUserButton;
-	  private WebElement emailField;
-	  private WebElement firstNameField;
-	  private WebElement lastNameField;
-	  private WebElement addButton;
-	  
-	//  private String seleniumHub = "http://192.168.60.135:4444/wd/hub";
-	
-	  private String baseUrl = "http://localhost:8080/usermanager";
 
-	  @Before
-	  public void openBrowser() {
-			String base = System.getProperty("app.baseurl");
-			if(base == null) {
-				base = baseUrl;
-			}
-	  
-	    
-	    //driver = new HtmlUnitDriver();
-	    //((HtmlUnitDriver)driver).setJavascriptEnabled(true);
-	    
-	
-		//try to get Selenium HUB and bas test URL from JVM parameters
-		//This should set on Jenkins
-		/* 
-		String hub =  System.getProperty("selenium.hub");
-		if(hub == null) {
-			hub  = seleniumHub;
+	private WebDriver driver;
+	private ScreenshotHelper screenshotHelper;
+	private WebDriverWait driverWait;
+
+	private WebElement addUserButton;
+	private WebElement emailField;
+	private WebElement firstNameField;
+	private WebElement lastNameField;
+	private WebElement addButton;
+
+	// The URL to the application
+	private String baseUrl = "http://localhost:8080/usermanager";
+
+	@Before
+	public void openBrowser() {
+		String base = System.getProperty("app.baseurl");
+		if (base == null) {
+			base = baseUrl;
 		}
-			
-		System.out.println("Selenium HUB : "  + hub);
-		System.out.println("The app URL : "  + base);
-		  
-	    URL hubUrl = null;
-	    try{
-	    	hubUrl = new URL(hub);
-	    }catch(Exception e){
-	    	
-	    }
-	    
-	    Capabilities cap = DesiredCapabilities.chrome();
-	    driver = new RemoteWebDriver(hubUrl, cap);
-	    */
-		  
-	    //Use local Chrome Browser for testing
-	    driver = new ChromeDriver();
-	    driverWait = new WebDriverWait(driver, 30);
-	    driver.get(base);
-	   // screenshotHelper = new ScreenshotHelper();
-	  }
-	  
-	  @After
-	  public void saveScreenshotAndCloseBrowser() throws IOException {
-	   // screenshotHelper.saveScreenshot("screenshot.png");
-	    driver.quit();
-	  }
-	  
-	  /*
-	   * This is the test case for the application
-	   */
-	  @Test
-	  public void pageTitleAfterSearchShouldBeginWithDrupal() throws IOException {
-		  
+
+		// Use local Chrome Browser for testing
+		// Create a Web driver to control the browser
+		driver = new ChromeDriver();
+		driverWait = new WebDriverWait(driver, 30);
+		driver.get(base);
+		
+		//Create screenshot
+		screenshotHelper = new ScreenshotHelper();
+	}
+
+	@After
+	public void saveScreenshotAndCloseBrowser() throws IOException {
+		screenshotHelper.saveScreenshot("screenshot.png");
+		driver.quit();
+	}
+
+	/*
+	 * This is the test case for the application
+	 */
+	@Test
+	public void pageTitleAfterSearchShouldBeginWithDrupal() throws IOException {
+
 		System.out.println("Page Title : " + driver.getTitle());
-	    assertEquals("The page title should equal user manager at the start of the test.", 
-			 "User Manager", driver.getTitle());
-	    
-	    addUserButton = driver.findElement(By.id("btnAddUser"));
-	    
-	    if (addUserButton.isDisplayed()) {
-	    	System.out.println("Add User button is displayed");
-	    	addUserButton.click();
-	    	
-	    	driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("email")));
-	    	emailField = driver.findElement(By.id("email"));
-	    	emailField.sendKeys("abc@xyz.com");
-	    	
-	    	firstNameField = driver.findElement(By.id("firstName"));
-	    	firstNameField.sendKeys("Aaron");
-	    	
-	    	lastNameField = driver.findElement(By.id("lastName"));
-	    	lastNameField.sendKeys("Smith");
-	    	try{
-	    		Thread.sleep(5000);
-	    	} catch (Exception e){
-	    		
-	    	}
-	    	addButton = driver.findElement(By.id("btnSubmit"));
-	    	addButton.click();  
-	    	try{
-	    		Thread.sleep(5000);
-	    	} catch (Exception e){
-	    		
-	    	}
-	    	driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("d_"+"abc@xyz.com")));
-	    	
-	    	WebElement deleteButton = driver.findElement(By.id("d_"+"abc@xyz.com"));
-	    	
-	    	deleteButton.click();
-	    	
-	    	try{
-	    		Thread.sleep(5000);
-	    	} catch (Exception e){
-	    		
-	    	}
-	    }
-	
-	  }
-	  
-	  private class ScreenshotHelper {
-	  
-	    public void saveScreenshot(String screenshotFileName) throws IOException {
-	      File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-	      FileUtils.copyFile(screenshot, new File(screenshotFileName));
-	    }
-	  }
+
+		//Step 1: Check the page title of the website
+		assertEquals("The page title should equal user manager at the start of the test.", "User Manager",
+				driver.getTitle());
+
+		//Step 2: Find the "Add New User" Button
+		addUserButton = driver.findElement(By.id("btnAddUser"));
+
+		if (addUserButton.isDisplayed()) {
+			System.out.println("Add User button is displayed");
+			
+			//Step 3: Click the Add new user Button
+			addUserButton.click();
+
+			//Step 4: Wait for the page load by check email input field
+			driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("email")));
+			
+			//Step 5: Fill the new user form 
+			
+			//Step 5.1 Find the email input box.
+			emailField = driver.findElement(By.id("email"));
+			
+			//Step 5.2 Set the email value.
+			emailField.sendKeys("abc@xyz.com");
+
+			//Step 5.3 Find the firstName field
+			firstNameField = driver.findElement(By.id("firstName"));
+			
+			//Step 5.4 Set first name value
+			firstNameField.sendKeys("Aaron");
+
+			//Step 5.5 Find last name field
+			lastNameField = driver.findElement(By.id("lastName"));
+			
+			//Step 5.6 Set last name value
+			lastNameField.sendKeys("Smith");
+			
+			try {
+				Thread.sleep(5000);
+			} catch (Exception e) {
+
+			}
+			
+			//Step 6: Find the submit button
+			addButton = driver.findElement(By.id("btnSubmit"));
+			
+			//Step 7: Click the submit button
+			addButton.click();
+			try {
+				Thread.sleep(5000);
+			} catch (Exception e) {
+
+			}
+			
+			//Step 8: Wait for page reload by checking the new email value.
+			driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("d_" + "abc@xyz.com")));
+
+			//Step 9: Find the delete button for the new record.
+			WebElement deleteButton = driver.findElement(By.id("d_" + "abc@xyz.com"));
+
+			//Step 10: Click on delete button to delete record.
+			deleteButton.click();
+
+			try {
+				Thread.sleep(5000);
+			} catch (Exception e) {
+
+			}
+			
+			//Step 11: Check if the new record was deleted
+			try {
+				deleteButton = driver.findElement(By.id("d_" + "abc@xyz.com"));
+				assertNotEquals("Failed to delete record", deleteButton,
+					null);
+			} catch (Exception e) {
+				
+			}
+		}
+
+	}
+
+	private class ScreenshotHelper {
+
+		public void saveScreenshot(String screenshotFileName) throws IOException {
+			File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshot, new File(screenshotFileName));
+		}
+	}
 
 }
